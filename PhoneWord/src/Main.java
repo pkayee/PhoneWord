0a7a3ece91154eb9a077bd8efb1f5a3b141c995f
pkaye <pixelfuzepic@gmail.com>
2026-02-13 11:49:28 +0800

    Update Main.java

--- PhoneWord/src/Main.java
+++ PhoneWord/src/Main.java
@@ -5,16 +5,9 @@
 ///  Date : 13/02/26
 ///
 ///
-
-
-
-
-
-
 import Packages.Storage;
 
 void main() {
-
     Storage Strings = new Storage();
     String outputString = "";
 
@@ -26,28 +19,20 @@ void main() {
     for (int i = 0; i < userInput.length(); i++ ) {
          if (Strings.nums[2].indexOf(stringChars[i], 0) != -1 ) {
              outputString += "2";
-
          } else if (Strings.nums[3].indexOf(stringChars[i], 0) != -1) {
              outputString += "3";
-
          } else if (Strings.nums[4].indexOf(stringChars[i], 0) != -1) {
              outputString += "4";
-
          } else if (Strings.nums[5].indexOf(stringChars[i], 0) != -1) {
              outputString += "5";
-
          } else if (Strings.nums[6].indexOf(stringChars[i], 0) != -1) {
              outputString += "6";
-
          } else if (Strings.nums[7].indexOf(stringChars[i], 0) != -1) {
              outputString += "7";
-
          } else if (Strings.nums[8].indexOf(stringChars[i], 0) != -1) {
              outputString += "8";
-
          } else if (Strings.nums[9].indexOf(stringChars[i], 0) != -1) {
              outputString += "9";
-
 
          } try {
              int stringAsInt = Integer.parseInt(stringChars[i]);
