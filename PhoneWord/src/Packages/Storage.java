1594281fa64ea8844adbc8c7666550304500d6dc
pkaye <pixelfuzepic@gmail.com>
2026-02-13 11:44:18 +0800

    Add files via upload

---
+++ PhoneWord/src/Packages/Storage.java
@@ -0,0 +1,16 @@
+package Packages;
+public class Storage {
+    public String[] nums = {
+            "",
+            "",
+            "abc, ABC",
+            "def, DEF",
+            "ghl, GHL",
+            "jkl, JKL",
+            "mno, MNO",
+            "pgrs, PQRS",
+            "tuv, TUV",
+            "wxyz, WXYZ"
+    };
+
+}
