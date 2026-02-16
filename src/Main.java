e36e3f2164c0a515dba7a3246f878eb8dd101073
pkaye <pixelfuzepic@gmail.com>
2026-02-16 19:16:20 +0800

    out and jar folders added

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+///
+///
+///  Name : Patrick Kiernan
+///  id : 30140770
+///  Date : 13/02/26
+///
+///
+
+import Packages.Storage;
+
+public class Main {
+    void main() {
+        Storage Strings = new Storage();
+        String outputString = "";
+
+        var userInput = IO.readln("Enter String Number! \n");
+        String[] stringChars = userInput.split(""); // splits user input chars into string array
+
+
+        // iterates through table of chars and matches it to strings in storage
+        for (int i = 0; i < userInput.length(); i++) {
+            if (Strings.nums[2].contains(stringChars[i])) {
+                outputString += "2";
+            } else if (Strings.nums[3].contains(stringChars[i])) {
+                outputString += "3";
+            } else if (Strings.nums[4].contains(stringChars[i])) {
+                outputString += "4";
+            } else if (Strings.nums[5].contains(stringChars[i])) {
+                outputString += "5";
+            } else if (Strings.nums[6].contains(stringChars[i])) {
+                outputString += "6";
+            } else if (Strings.nums[7].contains(stringChars[i])) {
+                outputString += "7";
+            } else if (Strings.nums[8].contains(stringChars[i])) {
+                outputString += "8";
+            } else if (Strings.nums[9].contains(stringChars[i])) {
+                outputString += "9";
+
+            }
+            try {
+                int stringAsInt = Integer.parseInt(stringChars[i]);
+                outputString += stringChars[i];
+            } catch (Exception e) {
+            }
+        }
+        IO.println(outputString);
+
+    }
+}
---
+++ src/Packages/Storage.java
@@ -0,0 +1,17 @@
+package Packages;
+public class Storage {
+    public String[] nums = {
+            "",
+            "",
+            "abc, ABC",
+            "def, DEF",
+            "ghi, GHI",
+            "jkl, JKL",
+            "mno, MNO",
+            "pgrs, PQRS",
+            "tuv, TUV",
+            "wxyz, WXYZ"
+    };
+
+}
+
