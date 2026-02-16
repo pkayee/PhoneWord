95f84bd7268f862a32eb5fe922481a68b68bc10b
pkaye <pixelfuzepic@gmail.com>
2026-02-16 19:26:38 +0800

    Update RunCode.bat

--- RunCode.bat
+++ RunCode.bat
@@ -5,12 +5,13 @@ REM Get the directory where this bat file is located
 set "ROOT_DIR=%~dp0"
 
 REM Build path to jar relative to root
-set "JAR_PATH=%ROOT_DIR%PhoneWord\out\artifacts\PhoneWord_jar\PhoneWord.jar"
+set "JAR_PATH=%ROOT_DIR%out\artifacts\PhoneWord_jar\PhoneWord.jar"
 
 REM Change to root directory
 cd /d "%ROOT_DIR%"
 
 REM Run the jar
 java -jar "%JAR_PATH%"
+
 
 pause
